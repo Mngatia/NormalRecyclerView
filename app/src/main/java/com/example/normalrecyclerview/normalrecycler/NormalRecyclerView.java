2657525542642fa93c0b535f6f541b2ee24fc567
Mngatia <nyawirahngatia@gmail.com>
2021-02-10 20:34:54 +0300

    Normal RecyclerView.

---
+++ app/src/main/java/com/example/normalrecyclerview/normalrecycler/NormalRecyclerView.java
@@ -0,0 +1,16 @@
+package com.example.normalrecyclerview.normalrecycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.normalrecyclerview.R;
+
+public class NormalRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_recycler_view);
+    }
+}
