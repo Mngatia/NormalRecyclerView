a113aea9dc7cb8a98ba7ab93e4c0ccb405f6dc2d
Mngatia <nyawirahngatia@gmail.com>
2021-02-11 20:45:16 +0300

    Normal RecyclerView.

--- app/src/main/java/com/example/normalrecyclerview/normalrecycler/NormalRecyclerView.java
+++ app/src/main/java/com/example/normalrecyclerview/normalrecycler/NormalRecyclerView.java
@@ -38,5 +38,26 @@ public class NormalRecyclerView extends AppCompatActivity {
     private void createListData() {
 
         //Method - adding data to recyclerview
+
+        Planet planet = new Planet("Earth", 150, 10, 12750);
+        planetArrayList.add(planet);
+        planet = new Planet("Jupiter", 778, 26, 14300);
+        planetArrayList.add(planet);
+        planet = new Planet("Mars", 228, 4, 6800);
+        planetArrayList.add(planet);
+        planet = new Planet("Pluto", 5900, 1, 2320);
+        planetArrayList.add(planet);
+        planet = new Planet("Venus", 108, 9, 12750);
+        planetArrayList.add(planet);
+        planet = new Planet("Saturn", 1429, 11, 12000);
+        planetArrayList.add(planet);
+        planet = new Planet("Mercury", 58, 4, 4900);
+        planetArrayList.add(planet);
+        planet = new Planet("Neptune", 4500, 12, 50500);
+        planetArrayList.add(planet);
+        planet = new Planet("Uranus", 2870, 9, 52400);
+        planetArrayList.add(planet);
+        adapter.notifyDataSetChanged();
+
     }
 }
--- app/src/main/java/com/example/normalrecyclerview/normalrecycler/PlanetAdapter.java
+++ app/src/main/java/com/example/normalrecyclerview/normalrecycler/PlanetAdapter.java
@@ -1,6 +1,7 @@
 package com.example.normalrecyclerview.normalrecycler;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -8,7 +9,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.normalrecyclerview.R;
+
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetHolder> {
 
@@ -25,26 +29,46 @@ public class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetHold
 
     @NonNull
     @Override
-    public PlanetAdapter.PlanetHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public PlanetHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.plant_layout_item,
+                parent, false);
+        return new PlanetHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PlanetAdapter.PlanetHolder holder, int position) {
+        Planet planet = planets.get(position);
+        holder.SetDetails(planet);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return planets.size();
     }
+
 //. Planet Holder
-    public class PlanetHolder extends RecyclerView.ViewHolder {
+    class PlanetHolder extends RecyclerView.ViewHolder {
 
         private TextView txtName, txtDistance, txtGravity, txtDiameter;
-        
+
         public PlanetHolder(@NonNull View itemView) {
             super(itemView);
+
+            txtName = itemView.findViewById(R.id.txtName);
+            txtDistance = itemView.findViewById(R.id.txtDistance);
+            txtGravity = itemView.findViewById(R.id.txtGravity);
+            txtDiameter = itemView.findViewById(R.id.txtDiameter);
+        }
+
+        void SetDetails(Planet planet){
+            txtName.setText(planet.getPlanetName());
+            txtDistance.setText(String.format(Locale.US,
+                    "Distance from sun : %d Million KM", planet.getDistanceFromSun()));
+            txtGravity.setText(String.format(Locale.US,
+                    "Surface Gravity : %d N/Kg", planet.getGravity()));
+            txtDiameter.setText(String.format(Locale.US,
+                    "Diameter : %d KM", planet.getDiameter()));
         }
     }
 }
